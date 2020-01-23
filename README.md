ad58e998af83f1b8e67d9073aef7b528b5c53e00
Matthew Neal <matthewneal1202@gmail.com>
2020-01-22 17:05:43 -0800

    initial commit

---
+++ README.md
@@ -0,0 +1,29 @@
+# italia-memoriam-mmxix
+
+After my girlfriend and I took a life changing trip to Italy in June 2019, I decided to design a website to showcase our experience. The core of the
+website are the image galleries of the various cities we visited, but they are presented through a fun and user friendly platform.
+
+link to project: https://github.com/MMNXII/Italia_Memoriam_MMXIX
+
+to view locally: 1.) download zip file
+		 2.) extract folder inside zip file to somewhere on your computer
+		 3.) open "html" folder, right click on "index.html" and open in a web browser (preferably Google Chrome)
+		 4.) enjoy!
+
+
+
+
+Sources:
+
+map of italy:
+https://mapswire.com/countries/italy/
+
+arrow home page down:
+https://www.freeiconspng.com/img/44420
+
+arrow down to galleries on location pages:
+<div>Icon made from <a href="http://www.onlinewebfonts.com/icon">Icon Fonts</a> is licensed by CC BY 3.0</div>
+
+
+
+
---
+++ scripts/caserta.js
@@ -0,0 +1,64 @@
+
+
+document.body.style.backgroundImage = "url('../images/caserta/background.jpg')";
+document.body.style.backgroundPosition = "center";
+document.body.style.backgroundRepeat = "no-repeat";
+document.body.style.backgroundSize = "fill";
+
+
+let header = document.getElementById("headerContainer");
+    header.style.opacity = "0";
+let introPara = document.getElementById("intro");
+    introPara.style.opacity = "0";
+let arrow = document.getElementById("arrow");
+    arrow.style.opacity = "0";
+
+    function headerFade() {
+        header.style.opacity = "1";
+        header.style.transitionProperty = "opacity";
+        header.style.transitionDuration = "3s";
+        header.style.transitionDelay = "2s";
+
+        function introParaFade() {
+            introPara.style.opacity = "1";
+            introPara.style.transitionProperty = "opacity";
+            introPara.style.transitionDuration = "3s";
+            introPara.style.transitionDelay = "4s";
+
+            function arrowFade() {
+                arrow.style.opacity = "1";
+                arrow.style.transitionProperty = "opacity";
+                arrow.style.transitionDuration = "3s";
+                arrow.style.transitionDelay = "15s";
+            }
+
+                arrowFade();
+            }
+        introParaFade();
+    }
+
+
+
+let a;
+let index = 1;
+showSlides(index);
+
+    function slides(n) {
+        showSlides(index += n);
+    }
+
+        function showSlides(n) {
+            let images = document.getElementsByClassName("imgContainer");
+                if (n > images.length) {
+                    index = 1;
+
+                } if (n < 1) {
+                    index = images.length;
+
+                } for (a = 0; a < images.length; a++) {
+                    images[a].style.display = "none";
+                }
+
+                images[index-1].style.display = "block";
+        }
+
