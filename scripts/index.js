49dc7d5aa464f6da256922a1299582a0662f7765
Matthew Neal <matthewneal1202@gmail.com>
2020-02-06 20:07:40 -0800

    responsive

--- scripts/index.js
+++ scripts/index.js
@@ -1,118 +1,118 @@
 
 
-// let header = document.getElementById("headerContainer");
-//     header.style.opacity = "0";
-// let introPara = document.getElementById("intro");
-//     introPara.style.opacity = "0";
-// let introPara2 = document.getElementById("intro2");
-//     introPara2.style.opacity = "0";
-// let arrow = document.getElementById("arrowContainer");
-//     arrow.style.opacity = "0"
+let header = document.getElementById("headerContainer");
+    header.style.opacity = "0";
+let introPara = document.getElementById("intro");
+    introPara.style.opacity = "0";
+let introPara2 = document.getElementById("intro2");
+    introPara2.style.opacity = "0";
+let arrow = document.getElementById("arrowContainer");
+    arrow.style.opacity = "0"
 
-//     function headerFade() {
-//         header.style.opacity = "1";
-//         header.style.transitionProperty = "opacity";
-//         header.style.transitionDuration = "3s";
+    function headerFade() {
+        header.style.opacity = "1";
+        header.style.transitionProperty = "opacity";
+        header.style.transitionDuration = "3s";
 
-//         function introParaFade() {
-//             introPara.style.opacity = "1";
-//             introPara.style.transitionProperty = "opacity";
-//             introPara.style.transitionDuration = "6s";
-//             introPara.style.transitionDelay = "2s";
+        function introParaFade() {
+            introPara.style.opacity = "1";
+            introPara.style.transitionProperty = "opacity";
+            introPara.style.transitionDuration = "6s";
+            introPara.style.transitionDelay = "2s";
 
-//             function introParaFade2() {
-//                 introPara2.style.opacity = "1";
-//                 introPara2.style.transitionProperty = "opacity";
-//                 introPara2.style.transitionDuration = "4s";
-//                 introPara2.style.transitionDelay = "10s";
-//                 arrow.style.opacity = "1";
-//                 arrow.style.transitionProperty = "opacity";
-//                 arrow.style.transitionDuration = "4s";
-//                 arrow.style.transitionDelay = "10s";
-//             }
-//             introParaFade2();
-//     }
-//     introParaFade();
-//     }
-
-
+            function introParaFade2() {
+                introPara2.style.opacity = "1";
+                introPara2.style.transitionProperty = "opacity";
+                introPara2.style.transitionDuration = "4s";
+                introPara2.style.transitionDelay = "10s";
+                arrow.style.opacity = "1";
+                arrow.style.transitionProperty = "opacity";
+                arrow.style.transitionDuration = "4s";
+                arrow.style.transitionDelay = "10s";
+            }
+            introParaFade2();
+    }
+    introParaFade();
+    }
 
 
 
-// let veniceMarker = document.getElementById("veniceMarker");
-//     veniceMarker.style.textAlign = "center";
-//     veniceMarker.style.fontFamily = "Lobster, cursive";
-// let venice = document.createTextNode("Venice");
-//     veniceMarker.addEventListener("mouseover", extendVenice);
-//     veniceMarker.addEventListener("mouseout", retractVenice);
-
-//     function extendVenice() {
-//         veniceMarker.appendChild(venice);
-//         veniceMarker.style.cursor = "pointer";
-//     }
-//     function retractVenice() {
-//         veniceMarker.removeChild(venice);
-//     }
 
 
-// let florenceMarker = document.getElementById("florenceMarker");
-//     florenceMarker.style.textAlign = "center";
-//     florenceMarker.style.fontFamily = "Ibarra Real Nova, serif";
-// let florence = document.createTextNode("Florence");
-//     florenceMarker.addEventListener("mouseover", extendFlorence);
-//     florenceMarker.addEventListener("mouseout", retractFlorence);
+let veniceMarker = document.getElementById("veniceMarker");
+    veniceMarker.style.textAlign = "center";
+    veniceMarker.style.fontFamily = "Lobster, cursive";
+let venice = document.createTextNode("Venice");
+    veniceMarker.addEventListener("mouseover", extendVenice);
+    veniceMarker.addEventListener("mouseout", retractVenice);
 
-//     function extendFlorence() {
-//         florenceMarker.appendChild(florence);
-//         florenceMarker.style.cursor = "pointer";
-//     }
-//     function retractFlorence() {
-//         florenceMarker.removeChild(florence);
-//     }
-
-// let romeMarker = document.getElementById("romeMarker");
-//     romeMarker.style.textAlign = "center";
-//     romeMarker.style.fontFamily = "Stoke, serif";
-// let rome = document.createTextNode("Rome");        
-//     romeMarker.addEventListener("mouseover", extendRome);
-//     romeMarker.addEventListener("mouseout", retractRome);
-
-//     function extendRome() {
-//         romeMarker.appendChild(rome);
-//         romeMarker.style.cursor = "pointer";
-//     }
-//     function retractRome() {
-//         romeMarker.removeChild(rome);
-//     }
+    function extendVenice() {
+        veniceMarker.appendChild(venice);
+        veniceMarker.style.cursor = "pointer";
+    }
+    function retractVenice() {
+        veniceMarker.removeChild(venice);
+    }
 
 
-// let casertaMarker = document.getElementById("casertaMarker");
-//     casertaMarker.style.textAlign = "center";
-//     casertaMarker.style.fontFamily = "IM Fell Great Primer SC, serif";    ;
-// let caserta = document.createTextNode("Caserta");
-//     casertaMarker.addEventListener("mouseover", extendCaserta);
-//     casertaMarker.addEventListener("mouseout", retractCaserta);
+let florenceMarker = document.getElementById("florenceMarker");
+    florenceMarker.style.textAlign = "center";
+    florenceMarker.style.fontFamily = "Ibarra Real Nova, serif";
+let florence = document.createTextNode("Florence");
+    florenceMarker.addEventListener("mouseover", extendFlorence);
+    florenceMarker.addEventListener("mouseout", retractFlorence);
 
-//     function extendCaserta() {
-//         casertaMarker.appendChild(caserta);
-//         casertaMarker.style.cursor = "pointer";
-//     }
-//     function retractCaserta() {
-//         casertaMarker.removeChild(caserta);
-//     }
+    function extendFlorence() {
+        florenceMarker.appendChild(florence);
+        florenceMarker.style.cursor = "pointer";
+    }
+    function retractFlorence() {
+        florenceMarker.removeChild(florence);
+    }
+
+let romeMarker = document.getElementById("romeMarker");
+    romeMarker.style.textAlign = "center";
+    romeMarker.style.fontFamily = "Stoke, serif";
+let rome = document.createTextNode("Rome");        
+    romeMarker.addEventListener("mouseover", extendRome);
+    romeMarker.addEventListener("mouseout", retractRome);
+
+    function extendRome() {
+        romeMarker.appendChild(rome);
+        romeMarker.style.cursor = "pointer";
+    }
+    function retractRome() {
+        romeMarker.removeChild(rome);
+    }
 
 
-// let amalfiMarker = document.getElementById("amalfiMarker");
-//     amalfiMarker.style.textAlign = "center";
-//     amalfiMarker.style.fontFamily = "Marck Script, cursive";
-// let amalfi = document.createTextNode("Amalfi");
-//     amalfiMarker.addEventListener("mouseover", extendAmalfi);
-//     amalfiMarker.addEventListener("mouseout", retractAmalfi);
+let casertaMarker = document.getElementById("casertaMarker");
+    casertaMarker.style.textAlign = "center";
+    casertaMarker.style.fontFamily = "IM Fell Great Primer SC, serif";    ;
+let caserta = document.createTextNode("Caserta");
+    casertaMarker.addEventListener("mouseover", extendCaserta);
+    casertaMarker.addEventListener("mouseout", retractCaserta);
 
-//     function extendAmalfi() {
-//         amalfiMarker.appendChild(amalfi);
-//         amalfiMarker.style.cursor = "pointer";
-//     }
-//     function retractAmalfi() {
-//         amalfiMarker.removeChild(amalfi);
-//     }
+    function extendCaserta() {
+        casertaMarker.appendChild(caserta);
+        casertaMarker.style.cursor = "pointer";
+    }
+    function retractCaserta() {
+        casertaMarker.removeChild(caserta);
+    }
+
+
+let amalfiMarker = document.getElementById("amalfiMarker");
+    amalfiMarker.style.textAlign = "center";
+    amalfiMarker.style.fontFamily = "Marck Script, cursive";
+let amalfi = document.createTextNode("Amalfi");
+    amalfiMarker.addEventListener("mouseover", extendAmalfi);
+    amalfiMarker.addEventListener("mouseout", retractAmalfi);
+
+    function extendAmalfi() {
+        amalfiMarker.appendChild(amalfi);
+        amalfiMarker.style.cursor = "pointer";
+    }
+    function retractAmalfi() {
+        amalfiMarker.removeChild(amalfi);
+    }
