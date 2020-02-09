d1d9a80f097e30bf2a87921c8ae47820b23529c7
Matthew Neal <matthewneal1202@gmail.com>
2020-02-09 13:48:38 -0800

    revised

--- scripts/venice.js
+++ scripts/venice.js
@@ -5,36 +5,36 @@ document.body.style.backgroundPosition = "center";
 document.body.style.backgroundRepeat = "no-repeat";
 document.body.style.backgroundSize = "fill";
 
-var header = document.getElementById("headerContainer");
-    header.style.opacity = "0";
-var introPara = document.getElementById("intro");
-    introPara.style.opacity = "0";
-var imgContainer = document.getElementById("imgContainer");
-    imgContainer.style.opacity = "0";
+// var header = document.getElementById("headerContainer");
+//     header.style.opacity = "0";
+// var introPara = document.getElementById("intro");
+//     introPara.style.opacity = "0";
+// var imgContainer = document.getElementById("imgContainer");
+//     imgContainer.style.opacity = "0";
 
-    function headerFade() {
-        header.style.opacity = "1";
-        header.style.transitionProperty = "opacity";
-        header.style.transitionDuration = "3s";
-        header.style.transitionDelay = "2s";
+//     function headerFade() {
+//         header.style.opacity = "1";
+//         header.style.transitionProperty = "opacity";
+//         header.style.transitionDuration = "3s";
+//         header.style.transitionDelay = "2s";
 
-        function introParaFade() {
-            introPara.style.opacity = "1";
-            introPara.style.transitionProperty = "opacity";
-            introPara.style.transitionDuration = "3s";
-            introPara.style.transitionDelay = "4s";
+//         function introParaFade() {
+//             introPara.style.opacity = "1";
+//             introPara.style.transitionProperty = "opacity";
+//             introPara.style.transitionDuration = "3s";
+//             introPara.style.transitionDelay = "4s";
 
-            function arrowFade() {
-                imgContainer.style.opacity = "1";
-                imgContainer.style.transitionProperty = "opacity";
-                imgContainer.style.transitionDuration = "3s";
-                imgContainer.style.transitionDelay = "22s";
-            }
+//             function arrowFade() {
+//                 imgContainer.style.opacity = "1";
+//                 imgContainer.style.transitionProperty = "opacity";
+//                 imgContainer.style.transitionDuration = "3s";
+//                 imgContainer.style.transitionDelay = "22s";
+//             }
 
-                arrowFade();
-            }
-        introParaFade();
-    }
+//                 arrowFade();
+//             }
+//         introParaFade();
+//     }
 
 
 function captionAppear(className) {
